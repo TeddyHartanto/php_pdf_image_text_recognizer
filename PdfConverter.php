9b3f6b95d1c85e4f0fde80fef5c04c5ed296b49c
Teddy Hartanto <hartantoteddy@yahoo.com>
2017-03-11 18:34:59 +0800

    Done with cropping

--- PdfConverter.php
+++ PdfConverter.php
@@ -8,15 +8,16 @@
  */
 
 $pdf_dir = 'pdfs/';
-$dir_items = array_diff(scandir($pdf_dir), ['.', '..']);
+//$dir_items = array_diff(scandir($pdf_dir), ['.', '..']);
+//
+//foreach ($dir_items as $dir_item) {
+//    $filename_no_ext = substr($dir_item, 0, -4);
+//    $input = $pdf_dir . $dir_item;
+//    $output = "jpgs/{$filename_no_ext}.jpg";
+//    hq_pdf_to_jpeg($input, 0, $output);
+//}
 
-
-foreach ($dir_items as $dir_item) {
-    $filename_no_ext = substr($dir_item, 0, -4);
-    $input = $pdf_dir . $dir_item;
-    $output = "jpgs/{$filename_no_ext}.jpg";
-    hq_pdf_to_jpeg($input, 0, $output);
-}
+extract_jpeg('jpgs/test.jpg', 'cropped_jpgs/test_code_number.jpg', 206, 51, 250, 187);
 
 // This code is to show: the image in a webpage
 //$imagick = new Imagick();
@@ -45,5 +46,23 @@ function hq_pdf_to_jpeg($input, $pageNum, $output) {
     $imagick->setImageCompressionQuality(100);
     $imagick->sharpenImage(0, 1.0);
     $imagick = $imagick->flattenImages();
+    $imagick->writeImage($output);
+}
+
+/**
+ * Equivalent to the following CLI command:
+ *      exec("convert {$input} -crop {$width}x{$height}+{$x}+{$y} {$output}");
+ *
+ * @param $input    -> input JPEG filename
+ * @param $output   -> output JPEG filename
+ * @param $width    -> width of the crop
+ * @param $height   -> height of the crop
+ * @param $x        -> top-left x coordinate of the desired cropped region
+ * @param $y        -> top left y coordinate of the desired cropped region
+ */
+function extract_jpeg($input, $output, $width, $height, $x, $y) {
+    $imagick = new Imagick();
+    $imagick->readImage($input);
+    $imagick->cropImage($width, $height, $x, $y);
     $imagick->writeImage($output);
 }
